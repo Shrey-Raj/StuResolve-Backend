8fa84d6f7921fd87a096c81e510b46388b374271
Shrey Raj <shreyraj1653@gmail.com>
2023-10-22 20:15:37 +0530

    update

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+const PORT = process.env.PORT || 3005 ;
 
 mongoose.set("strictQuery", false);
-// const MONGO_URI = 'mongodb+srv://shreyraj:mongo12345@cluster0.kdadybz.mongodb.net/' ;
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = 'mongodb+srv://shreyraj:mongo12345@cluster0.kdadybz.mongodb.net/' ;
+// const MONGO_URI = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
